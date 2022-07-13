f779e0d31346af1768e8b84fb332bd7e45666829
Konrad Weihmann <kweihmann@outlook.com>
2022-07-13 09:18:21 +0200

    Release 2.3
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="pysymbolcheck",
-    version="2.2",
+    version="2.3",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="ELF symbol check",
