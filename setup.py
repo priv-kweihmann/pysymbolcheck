b5eb5e2f78c55e5ae1244a42389c8afc1e5f5e31
priv-kweihmann <priv-kweihmann@localhost>
2023-03-15 07:09:26 +0000

    Release 2.7.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="pysymbolcheck",
-    version="2.7.3",
+    version="2.7.4",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="ELF symbol check",
